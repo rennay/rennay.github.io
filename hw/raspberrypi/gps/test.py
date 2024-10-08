90cfda1337c0210629025dfe31c72ef117b35e56
rennay <rennay@gmail.com>
2024-10-09 00:11:07 +0200

    Add files via upload
    Updated for python3

--- hw/raspberrypi/gps/test.py
+++ hw/raspberrypi/gps/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 import json
-from gps3 import gps3 
-#from google.cloud import pubsub_v1
+from gps3 import gps3
+# from google.cloud import pubsub_v1
 from dateutil.parser import parse
 from datetime import datetime
 import pytz
@@ -18,19 +18,17 @@ if __name__ == '__main__':
     gps_socket.watch()
     old_time = datetime.utcnow().replace(tzinfo=pytz.UTC)
     for new_data in gps_socket:
-	  if new_data:
-	  	data_stream.unpack(new_data)
-		data = {"altitude":data_stream.TPV['alt'], "latitude":data_stream.TPV['lat'], "longitude":data_stream.TPV['lon'], "speed":data_stream.TPV['speed'],"time":data_stream.TPV['time']}
-		if (data_stream.TPV['time'] != 'n/a'):
-			new_time = parse(data_stream.TPV['time'])
+        if new_data:
+            data_stream.unpack(new_data)
+            data = {"altitude": data_stream.TPV['alt'], "latitude": data_stream.TPV['lat'],
+                    "longitude": data_stream.TPV['lon'], "speed": data_stream.TPV['speed'], "time": data_stream.TPV['time']}
+            if (data_stream.TPV['time'] != 'n/a'):
+                new_time = parse(data_stream.TPV['time'])
 
-			diff_time = new_time - old_time
-			if (diff_time.total_seconds() >= 10):
-				old_time = new_time
-				print new_time, '-', data
-				json_string = json.dumps(data)
-				json_string = json_string.encode('utf-8')
-#				publisher.publish(topic_path, data=json_string)
-
-
-	
+                diff_time = new_time - old_time
+                if (diff_time.total_seconds() >= 10):
+                    old_time = new_time
+                    print(new_time, '-', data)
+                    json_string = json.dumps(data)
+                    json_string = json_string.encode('utf-8')
+# publisher.publish(topic_path, data=json_string)
